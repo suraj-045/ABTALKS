b2ffdbc1eaa01e8ee7071d955b1fdc140e7e9fbc
Suraj <suraj@Surajs-MacBook-Air.local>
2026-02-02 19:50:45 +0530

    Challenge1 Added

---
+++ Challenge1.py
@@ -0,0 +1,12 @@
+# Taking Input from the user
+name = input("Enter your name:")
+age = input("Enter your age: ")
+city = input("Enter your city:")
+
+print("\n--- user Details ---")
+print("Name:", name)
+print("Age:",age)
+print("City: ",city)
+
+print("\nHello", name,"!")
+print("You are", age, "years old and you live in",city)
